e5cdea271dc026fb3015457b6a57d4db9178e610
Stuti Dutta <stutidutta@aarkglobalinc.com>
2024-06-05 10:52:59 +0530

    Add project files.

---
+++ WpfApp3/View/UserControl/MenuBar.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp3.View.UserControl
+{
+    /// <summary>
+    /// Interaction logic for MenuBar.xaml
+    /// </summary>
+    public partial class MenuBar 
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
